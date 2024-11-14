560ce3a7562633b464f8f27247b7b01204414164
Sandaru Kaushan <sandarubusiness1001@gmail.com>
2024-11-14 09:39:32 +0530

    add comment SnakeGame.java file

--- Snake Game/SnakeGame.java
+++ Snake Game/SnakeGame.java
@@ -3,12 +3,18 @@ import javax.swing.JFrame;
 
 public class SnakeGame {
     public static void main(String[] args) {
+        // Create a new JFrame object with the title "Snake Game with AI"
         JFrame frame = new JFrame("Snake Game with AI");
+        // Create a new GamePanel object
         GamePanel panel = new GamePanel();
 
+        // Add the GamePanel object to the JFrame
         frame.add(panel);
+        // Set the default close operation to exit the program
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Set the size of the JFrame to 600x600 pixels
         frame.setSize(600, 600);
+        // Make the JFrame visible
         frame.setVisible(true);
     }
 }
